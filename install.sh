2bb1ca52ba47389a6ae2ad0630bd6c028727d624
David Hain <57726396+d-hain@users.noreply.github.com>
2023-10-04 08:43:57 +0200

    fix: many things removed and fixed

--- install.sh
+++ install.sh
@@ -28,61 +28,53 @@ read _button
 
 
 # Update system with apt
-apt update && apt upgrade
+sudo apt update && apt upgrade
 
 # Setting up Brave install
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-apt update
+sudo apt update
 
 # Installing all packages
-apt install \
+sudo apt install \
     brave-browser \
-	rust-all \
-	zsh alacritty \
-	vim neovim \
-	i3 \
-	docker docker-compose \
-	virt-manager \
-	neofetch imv \
-	exa btop bat ripgrep \
-	brightnessctl pavucontrol bluez pamixer \
-	nautilus filezilla libreoffice \
-	mypaint \
+    rust-all \
+    zsh alacritty \
+    vim neovim \
+    i3 i3lock \
+    docker docker-compose \
+    virt-manager \
+    neofetch imv \
+    exa btop bat ripgrep \
+    brightnessctl pavucontrol bluez pamixer \
+    nautilus filezilla libreoffice \
+    mypaint \
     fonts-jetbrains-mono fonts-font-awesome \
-	lolcat sl \
-    grim i3lock
+    lolcat sl \
+    grim
 
-
-# Updating Rust
-rustup update
 
 # Installing Discord
-curl "https://discord.com/api/download/canary?platform=linux&format=deb" --output discord.deb
-apt install discord.deb
-
-# Installing Synology Drive
-# TODO: URL not working
-curl "https://global.synologydownload.com/download/Utility/SynologyDriveClient/3.3.0-15082/Ubuntu/Installer/x86_64/synology-drive-client-15082.x86_64.deb?model=DS218%2B&bays=2&dsm_version=7.2.1&build_number=69057" --output synology-drive.deb
+sudo curl "https://dl-canary.discordapp.net/apps/linux/0.0.169/discord-canary-0.0.169.deb" --output discord.deb
+sudo apt install ./discord.deb
 
 # Installing JetBrains Toolbox
 jetbrainsfiledir=$(pwd)
 wget -cO jetbrains-toolbox.tar.gz "https://data.services.jetbrains.com/products/download?platform=linux&code=TBA"
 cd /usr/bin/
-tar -xzf $jetbrainsfiledir/jetbrains-toolbox.tar.gz
-mv jetbrains-toolbox-* jetbrains-toolbox
+sudo tar -xzf $jetbrainsfiledir/jetbrains-toolbox.tar.gz
+sudo mv jetbrains-toolbox-* jetbrains-toolbox
 cd $jetbrainsfiledir
 
 # Installing Postman
 postmanfiledir=$(pwd)
 wget -cO postman.tar.gz "https://dl.pstmn.io/download/latest/linux_64"
 cd /usr/bin/
-tar -xzf $postmanfiledir/postman.tar.gz
+sudo tar -xzf $postmanfiledir/postman.tar.gz
 cd $postmanfiledir
 
 # Installing Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 
 # Installing ohmyzsh plugins
 zshcustom=$HOME/.oh-my-zsh/custom
@@ -113,6 +105,8 @@ workdir=$(pwd)
 # .config
 sudo mv $workdir/dotfiles/.config/alacritty   $homedir/.config/alacritty
 sudo mv $workdir/dotfiles/.config/nvim        $homedir/.config/nvim
+sudo mv $workdir/dotfiles/.config/i3        $homedir/.config/i3
+sudo mv $workdir/dotfiles/.config/i3status        $homedir/.config/i3status
 
 sudo mv $workdir/dotfiles/.vimrc              $homedir/
 sudo mv $workdir/dotfiles/.ideavimrc          $homedir/
