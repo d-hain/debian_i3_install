b1aa9a096898900317abd6287dbb50e058d9afef
David Hain <57726396+d-hain@users.noreply.github.com>
2023-10-05 22:06:21 +0200

    feat: created script for installing ohmyzsh plugins

---
+++ oh-my-zsh-plugins.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+# Installing ohmyzsh plugins
+zshcustom=$HOME/.oh-my-zsh/custom
+git clone https://github.com/zsh-users/zsh-autosuggestions $zshcustom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $zshcustom/plugins/zsh-syntax-highlighting
