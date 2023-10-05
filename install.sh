141eb70df6c26703cb6c399db6ec6e02830a5ceb
David Hain <57726396+d-hain@users.noreply.github.com>
2023-10-05 21:02:14 +0200

    feat: installing curl

--- install.sh
+++ install.sh
@@ -30,6 +30,9 @@ read _button
 # Update system with apt
 sudo apt update && apt upgrade
 
+# Installing curl
+sudo apt install curl
+
 # Setting up Brave install
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
